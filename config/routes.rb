8abfd844a9f1ff283af389104d802f00b7ab55d5
Sammy Owen <ourcityourmayor@gmail.com>
2017-01-09 20:16:45 -0500

    map requestes to welcome/index not home/index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  root 'home#index'
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
